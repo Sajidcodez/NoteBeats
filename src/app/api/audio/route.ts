b8b98906a14127f032bcf133f2cb2e4420519bee
DarthCoder501 <peguerojoshua67@gmail.com>
2025-04-26 16:27:25 -0400

    "Audio file for lyrics"

---
+++ src/app/api/audio/route.ts
@@ -0,0 +1,38 @@
+import { ElevenLabsClient } from "elevenlabs";
+import { Readable } from "stream";
+import "dotenv/config";
+
+export const POST = async (req: Request) => {
+  try {
+    const body = await req.json();
+    const lyrics = body.lyrics;
+
+    const client = new ElevenLabsClient();
+
+    const audio = await client.textToSpeech.convert("ui0NMIinCTg8KvB4ogeV", {
+      text: lyrics,
+      model_id: "eleven_multilingual_v2",
+      output_format: "mp3_44100_128",
+    });
+
+    const chunks: Uint8Array[] = [];
+    for await (const chunk of audio as Readable) {
+      chunks.push(chunk as Uint8Array);
+    }
+    const audioBuffer = Buffer.concat(chunks);
+
+    const fastapiResponse = await fetch("https://audiomerger.onrender.com", {
+      method: "POST",
+      headers: {
+        "Content-Type": "audio/mpeg",
+        "Content-Length": audioBuffer.length.toString(),
+      },
+      body: audioBuffer,
+    });
+
+    return new Response("Audio uploaded successfully!");
+  } catch (error) {
+    console.error("Error generating audio:", error);
+    return new Response("Error generating audio", { status: 500 });
+  }
+};
