34460c6e5eb28bc93d9edae713339d6cec5ff8c8
DarthCoder501 <peguerojoshua67@gmail.com>
2025-04-26 11:54:35 -0400

    "env variables for s3 bucket & formatted code"

--- environment.d.ts
+++ environment.d.ts
@@ -1,14 +1,13 @@
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-        NODE_ENV: 'development' | 'production' ;
-        BUCKET_NAME: string;
-        REGION: string;
-        ACCESS_KEY_ID: string;
-        SECRET_ACCESS_KEY: string;
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: "development" | "production";
+      BUCKET_NAME: string;
+      REGION: string;
+      ACCESS_KEY_ID: string;
+      SECRET_ACCESS_KEY: string;
     }
+  }
 }
 
-export {} ;
-
+export {};
